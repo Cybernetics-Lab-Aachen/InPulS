af7855b34a52b6f07871acf96a54d83db519385e
Jascha Bakarinow <jascha.bakarinow@ifu.rwth-aachen.de>
2016-05-01 10:53:43 +0200

    moved iiwa specific code to its own place and restored modified pr2 specific code

--- python/gps/agent/config.py
+++ python/gps/agent/config.py
@@ -45,6 +45,33 @@ try:
         #TODO: Actually pass in low gains and high gains and use both
         #      for the position controller.
         'pid_params': np.array([
+            2400.0, 0.0, 18.0, 4.0,
+            1200.0, 0.0, 20.0, 4.0,
+            1000.0, 0.0, 6.0, 4.0,
+            700.0, 0.0, 4.0, 4.0,
+            300.0, 0.0, 6.0, 2.0,
+            300.0, 0.0, 4.0, 2.0,
+            300.0, 0.0, 2.0, 2.0
+        ]),
+    }
+
+    # AgentROSIIWA
+    AGENT_ROS_IIWA = {
+        #TODO: It might be worth putting this in JSON/yaml format so C++
+        #      can read it.
+        'trial_command_topic': 'gps_controller_trial_command',
+        'reset_command_topic': 'gps_controller_position_command',
+        'relax_command_topic': 'gps_controller_relax_command',
+        'data_request_topic': 'gps_controller_data_request',
+        'sample_result_topic': 'gps_controller_report',
+        'trial_timeout': 20,  # Give this many seconds for a trial.
+        'reset_conditions': [],  # Defines reset modes + positions for
+                                 # trial and auxiliary arms.
+        'frequency': 20,
+        'end_effector_points': np.array([]),
+        #TODO: Actually pass in low gains and high gains and use both
+        #      for the position controller.
+        'pid_params': np.array([
 	    2400.0, 0.0, 18.0, 4.0,
             1000.0, 0.0, 200.0, 4.0,
             1000.0, 0.0, 6.0, 4.0,
@@ -56,9 +83,11 @@ try:
     }
 except ImportError as e:
     AGENT_ROS = {}
+    AGENT_ROS_IIWA = {}
     LOGGER.debug('No ROS enabled: %s', e)
 except rospkg.common.ResourceNotFound as e:
     AGENT_ROS = {}
+    AGENT_ROS_IIWA = {}
     LOGGER.debug('No gps_agent_pkg: %s', e)
 
 
