21f6c4aa9ed416d3c3635560bce3beac2078ede0
Jascha Bakarinow <jascha.bakarinow@ifu.rwth-aachen.de>
2017-07-25 18:48:21 +0200

    Removed unneede files

--- python/gps/agent/config.py
+++ python/gps/agent/config.py
@@ -1,6 +1,5 @@
 """ Default configuration and hyperparameters for agent objects. """
 import logging
-
 import numpy as np
 
 
@@ -23,83 +22,24 @@ AGENT = {
 
 try:
     import rospkg
-
     import roslib
-
     roslib.load_manifest('gps_agent_pkg')
 
-    # AgentROS
-    AGENT_ROS = {
-        #TODO: It might be worth putting this in JSON/yaml format so C++
-        #      can read it.
-        'trial_command_topic': 'gps_controller_trial_command',
-        'reset_command_topic': 'gps_controller_position_command',
-        'relax_command_topic': 'gps_controller_relax_command',
-        'data_request_topic': 'gps_controller_data_request',
-        'sample_result_topic': 'gps_controller_report',
-        'trial_timeout': 20,  # Give this many seconds for a trial.
-        'reset_conditions': [],  # Defines reset modes + positions for
-                                 # trial and auxiliary arms.
-        'frequency': 20,
-        'end_effector_points': np.array([]),
-        #TODO: Actually pass in low gains and high gains and use both
-        #      for the position controller.
-        'pid_params': np.array([
-            2400.0, 0.0, 18.0, 4.0,
-            1200.0, 0.0, 20.0, 4.0,
-            1000.0, 0.0, 6.0, 4.0,
-            700.0, 0.0, 4.0, 4.0,
-            300.0, 0.0, 6.0, 2.0,
-            300.0, 0.0, 4.0, 2.0,
-            300.0, 0.0, 2.0, 2.0
-        ]),
-    }
-
-    # AgentROSIIWA
-    AGENT_ROS_IIWA = {
-        #TODO: It might be worth putting this in JSON/yaml format so C++
-        #      can read it.
-        'trial_command_topic': 'gps_controller_trial_command',
-        'reset_command_topic': 'gps_controller_position_command',
-        'relax_command_topic': 'gps_controller_relax_command',
-        'data_request_topic': 'gps_controller_data_request',
-        'sample_result_topic': 'gps_controller_report',
-        'trial_timeout': 20,  # Give this many seconds for a trial.
-        'reset_conditions': [],  # Defines reset modes + positions for
-                                 # trial and auxiliary arms.
-        'frequency': 20,
-        'end_effector_points': np.array([]),
-        #TODO: Actually pass in low gains and high gains and use both
-        #      for the position controller.
-        'pid_params': np.array([
-	    2400.0, 0.0, 18.0, 4.0,
-            1000.0, 0.0, 200.0, 4.0,
-            1000.0, 0.0, 6.0, 4.0,
-            700.0, 0.0, 4.0, 4.0,
-            300.0, 0.0, 6.0, 2.0,
-            300.0, 0.0, 4.0, 2.0,
-	    10.0, 0.1, 1.0, 2.0
-        ]),
-    }
 
     # AgentROSJACO
     AGENT_ROS_JACO = {
-        #TODO: It might be worth putting this in JSON/yaml format so C++
-        #      can read it.
         'trial_command_topic': 'gps_controller_trial_command',
         'reset_command_topic': 'gps_controller_position_command',
         'relax_command_topic': 'gps_controller_relax_command',
         'data_request_topic': 'gps_controller_data_request',
         'sample_result_topic': 'gps_controller_report',
-        'trial_timeout': 20,  # Give this many seconds for a trial.
-        'reset_conditions': [],  # Defines reset modes + positions for
-                                 # trial and auxiliary arms.
+        'trial_timeout': 20,    # Give this many seconds for a trial.
+        'reset_conditions': [], # Defines reset modes + positions for
+                                # trial and auxiliary arms.
         'frequency': 20,
         'end_effector_points': np.array([]),
-        #TODO: Actually pass in low gains and high gains and use both
-        #      for the position controller.
         'pid_params': np.array([
-	    120.0, 0.0, 20.0, 4.0,
+            120.0, 0.0, 20.0, 4.0,
             100.0, 0.0, 10.0, 4.0,
             70.0, 0.0, 4.0, 4.0,
             30.0, 0.0, 6.0, 2.0,
@@ -107,28 +47,10 @@ try:
             30.0, 0.0, 2.0, 2.0
         ]),
     }
+
 except ImportError as e:
-    AGENT_ROS = {}
-    AGENT_ROS_IIWA = {}
     AGENT_ROS_JACO = {}
     LOGGER.debug('No ROS enabled: %s', e)
 except rospkg.common.ResourceNotFound as e:
-    AGENT_ROS = {}
-    AGENT_ROS_IIWA = {}
     AGENT_ROS_JACO = {}
     LOGGER.debug('No gps_agent_pkg: %s', e)
-
-
-# AgentMuJoCo
-AGENT_MUJOCO = {
-    'substeps': 1,
-    'camera_pos': np.array([2., 3., 2., 0., 0., 0.]),
-    'image_width': 640,
-    'image_height': 480,
-    'image_channels': 3,
-    'meta_include': []
-}
-
-AGENT_BOX2D = {
-    'render': True,
-}
