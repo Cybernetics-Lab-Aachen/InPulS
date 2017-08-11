1d039ffc581da2f3992eca2e64988be8a42580f8
Jascha Bakarinow <jascha.bakarinow@ifu.rwth-aachen.de>
2017-08-11 20:34:04 +0200

    Small changes to dynamics fit code

--- python/gps/algorithm/dynamics/dynamics_lr_prior.py
+++ python/gps/algorithm/dynamics/dynamics_lr_prior.py
@@ -26,68 +26,57 @@ class DynamicsLRPrior(Dynamics):
 
     def fit(self, X, U):
         """ Fit dynamics. """
+        # Constants
         N, T, dimX = X.shape
         dimU = U.shape[2]
 
-        if N == 1:
-            raise ValueError("Cannot fit dynamics on 1 sample")
+        index_xu = slice(dimX + dimU)
+        index_x = slice(dimX + dimU, dimX + dimU + dimX)
+
+        sig_reg = np.zeros((dimX + dimU + dimX, dimX + dimU + dimX))
+        sig_reg[index_xu, index_xu] = self._hyperparams['regularization']
+
+        # Weights used in computing sample mean and sample covariance.
+        dwts = (1.0 / N) * np.ones(N)
+        D = np.diag(dwts)
 
         # Allocate 
         self.Fm = np.zeros([T, dimX, dimX + dimU])
         self.fv = np.zeros([T, dimX])
         self.dyn_covar = np.zeros([T, dimX, dimX])
 
-        index_XU = slice(dimX + dimU)
-
-        sig_reg = np.zeros((dimX + dimU + dimX, dimX + dimU + dimX))
-        sig_reg[index_XU, index_XU] = self._hyperparams['regularization']
-
-        # Fit dynamics with least squares regression.
-        dwts = (1.0 / N) * np.ones(N)
         for t in range(T - 1):
             Ys = np.c_[X[:, t, :], U[:, t, :], X[:, t + 1, :]]
 
-            # Obtain Normal-inverse-Wishart prior.
+            # Obtain normal-inverse-Wishart prior.
             mu0, Phi, mm, n0 = self.prior.eval(dimX, dimU, Ys)
 
-            # Obtain posterior and condition on [X, U]
-            Fm, fv, dyn_covar = self.gauss_fit_joint_prior(Ys,
-                        mu0, Phi, mm, n0, dwts, dimX, dimU, sig_reg)
+            # Compute empirical mean and covariance.
+            empmu = np.sum((Ys.T * dwts).T, axis=0)
+            diff = Ys - empmu
+            empsig = diff.T.dot(D).dot(diff)
+            # Symmetrize empsig to counter numerical errors.
+            empsig = 0.5 * (empsig + empsig.T)
+
+            # Compute posterior estimates of mean and covariance.
+            mu = empmu
+            sigma = (Phi + N * empsig + (N * mm) / (N + mm) *
+                     np.outer(empmu - mu0, empmu - mu0)) / (N + n0)
+            # Symmetrize sigma to counter numerical errors.
+            sigma = 0.5 * (sigma + sigma.T)
+            # Add sigma regularization.
+            sigma += sig_reg
+
+            # Conditioning to get dynamics.
+            Fm = np.linalg.solve(sigma[index_xu, index_xu],
+                                 sigma[index_xu, index_x]).T
+            fv = mu[index_x] - Fm.dot(mu[index_xu])
+            dyn_covar = sigma[index_x, index_x] - Fm.dot(sigma[index_xu, index_xu]).dot(Fm.T)
+            # Symmetrize dyn_covar to counter numerical errors.
+            dyn_covar = 0.5 * (dyn_covar + dyn_covar.T)
 
             self.Fm[t, :, :] = Fm
             self.fv[t, :] = fv
             self.dyn_covar[t, :, :] = dyn_covar
+
         return self.Fm, self.fv, self.dyn_covar
-
-    def gauss_fit_joint_prior(self, Ys, mu0, Phi, m, n0, dwts, dimX, dimU, sig_reg):
-        """ Perform Gaussian fit to data with a prior. """
-        # Build weights matrix.
-        D = np.diag(dwts)
-        # Compute empirical mean and covariance.
-        mun = np.sum((Ys.T * dwts).T, axis=0)
-        diff = Ys - mun
-        empsig = diff.T.dot(D).dot(diff)
-        # Symmetrize empsig to counter numerical errors.
-        empsig = 0.5 * (empsig + empsig.T)
-
-        # MAP estimate of joint distribution.
-        N = dwts.shape[0]
-        mu = mun
-        sigma = (N * empsig + Phi + (N * m) / (N + m) *
-                 np.outer(mun - mu0, mun - mu0)) / (N + n0)
-        # Symmetrize sigma to counter numerical errors.
-        sigma = 0.5 * (sigma + sigma.T)
-        # Add sigma regularization.
-        sigma += sig_reg
-
-        # Conditioning to get dynamics.
-        index_XU = slice(dimX + dimU)
-        index_X = slice(dimX + dimU, dimX + dimU + dimX)
-        Fm = np.linalg.solve(sigma[index_XU, index_XU],
-                             sigma[index_XU, index_X]).T
-        fv = mu[index_X] - Fm.dot(mu[index_XU])
-        dyn_covar = sigma[index_X, index_X] - Fm.dot(sigma[index_XU, index_XU]).dot(Fm.T)
-        # Symmetrize dyn_covar to counter numerical errors.
-        dyn_covar = 0.5 * (dyn_covar + dyn_covar.T)
-
-        return Fm, fv, dyn_covar
