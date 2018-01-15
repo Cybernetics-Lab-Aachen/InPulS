ee819bbdf4be9a9663933f405a5b5aa6f8a8438d
Jascha Bakarinow <jascha.bakarinow@ifu.rwth-aachen.de>
2018-01-15 16:34:34 +0100

    Restored badmm algorithm (tensorflow)

--- python/gps/algorithm/algorithm_utils.py
+++ python/gps/algorithm/algorithm_utils.py
@@ -87,3 +87,28 @@ def estimate_moments(X, mu, covar):
             covar_pad = np.vstack([pad1, np.hstack([pad2, covar[n, t, :, :]])])
             em[n, t, :, :] = np.outer(Xmu[n, t, :], Xmu[n, t, :]) + covar_pad
     return ev, em
+
+
+def gauss_fit_joint_prior(pts, mu0, Phi, m, n0, dwts, dX, dU, sig_reg):
+    """ Perform Gaussian fit to data with a prior. """
+    # Build weights matrix.
+    D = np.diag(dwts)
+    # Compute empirical mean and covariance.
+    mun = np.sum((pts.T * dwts).T, axis=0)
+    diff = pts - mun
+    empsig = diff.T.dot(D).dot(diff)
+    empsig = 0.5 * (empsig + empsig.T)
+    # MAP estimate of joint distribution.
+    N = dwts.shape[0]
+    mu = mun
+    sigma = (N * empsig + Phi + (N * m) / (N + m) *
+             np.outer(mun - mu0, mun - mu0)) / (N + n0)
+    sigma = 0.5 * (sigma + sigma.T)
+    # Add sigma regularization.
+    sigma += sig_reg
+    # Conditioning to get dynamics.
+    fd = np.linalg.solve(sigma[:dX, :dX], sigma[:dX, dX:dX+dU]).T
+    fc = mu[dX:dX+dU] - fd.dot(mu[:dX])
+    dynsig = sigma[dX:dX+dU, dX:dX+dU] - fd.dot(sigma[:dX, :dX]).dot(fd.T)
+    dynsig = 0.5 * (dynsig + dynsig.T)
+    return fd, fc, dynsig
