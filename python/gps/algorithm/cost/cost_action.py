eadbd30faa7078efb34ebb44ff9a69a1b9bf13cb
Philipp Ennen <philipp.ennen@gmail.com>
2018-01-15 12:26:18 +0100

    GCM WORKSgit add gcm/gcm.py gcm/gcm_utils.py gcm/nets/networks.py git add gcm/gcm.py gcm/gcm_utils.py gcm/nets/networks.py !

--- python/gps/algorithm/cost/cost_action.py
+++ python/gps/algorithm/cost/cost_action.py
@@ -31,3 +31,18 @@ class CostAction(Cost):
         lxx = np.zeros((T, Dx, Dx))
         lux = np.zeros((T, Du, Dx))
         return l, lx, lu, lxx, luu, lux
+
+    def eval_mu(self, mu, T, Du, Dx):
+        """
+        Evaluate cost function and derivatives on a sample.
+        Args:
+            sample: A single sample
+        """
+        sample_u = mu[:, slice(Dx, Dx + Du)]
+        l = 0.5 * np.sum(self._hyperparams['wu'] * (sample_u ** 2), axis=1)
+        lu = self._hyperparams['wu'] * sample_u
+        lx = np.zeros((T, Dx))
+        luu = np.tile(np.diag(self._hyperparams['wu']), [T, 1, 1])
+        lxx = np.zeros((T, Dx, Dx))
+        lux = np.zeros((T, Du, Dx))
+        return l, lx, lu, lxx, luu, lux
--- python/gps/algorithm/cost/cost_state.py
+++ python/gps/algorithm/cost/cost_state.py
@@ -33,6 +33,8 @@ class CostState(Cost):
         final_lux = np.zeros((T, Du, Dx))
 
         for data_type in self._hyperparams['data_types']:
+            print("data_type: ", data_type)
+
             config = self._hyperparams['data_types'][data_type]
             wp = config['wp']
             tgt = config['target_state']
@@ -60,4 +62,46 @@ class CostState(Cost):
             sample.agent.pack_data_x(final_lx, ls, data_types=[data_type])
             sample.agent.pack_data_x(final_lxx, lss,
                                      data_types=[data_type, data_type])
+        return final_l, final_lx, final_lu, final_lxx, final_luu, final_lux
+
+    def eval_mu(self, mu, T, Du, Dx):
+        """
+        Evaluate cost function and derivatives on a sample.
+        Args:
+            sample:  A single sample
+        """
+
+        final_l = np.zeros(T)
+        final_lu = np.zeros((T, Du))
+        final_lx = np.zeros((T, Dx))
+        final_luu = np.zeros((T, Du, Du))
+        final_lxx = np.zeros((T, Dx, Dx))
+        final_lux = np.zeros((T, Du, Dx))
+
+        for data_type in self._hyperparams['data_types']:
+            print("data_type: ", data_type)
+
+            config = self._hyperparams['data_types'][data_type]
+            wp = config['wp']
+            tgt = config['target_state']
+            x = mu[:, 0:6]
+            _, dim_sensor = x.shape
+
+            wpm = get_ramp_multiplier(
+                self._hyperparams['ramp_option'], T,
+                wp_final_multiplier=self._hyperparams['wp_final_multiplier']
+            )
+            wp = wp * np.expand_dims(wpm, axis=-1)
+            # Compute state penalty.
+            dist = x - tgt
+
+            # Evaluate penalty term.
+            l, ls, lss = evall1l2term(
+                wp, dist, np.tile(np.eye(dim_sensor), [T, 1, 1]),
+                np.zeros((T, dim_sensor, dim_sensor, dim_sensor)),
+                self._hyperparams['l1'], self._hyperparams['l2'],
+                self._hyperparams['alpha']
+            )
+
+            final_l += l
         return final_l, final_lx, final_lu, final_lxx, final_luu, final_lux
