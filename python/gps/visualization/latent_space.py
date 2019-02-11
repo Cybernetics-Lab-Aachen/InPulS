013ec1752c0fee080d3804d65cdb27d2f4aad9b6
Robin Kupper <robin.kupper@rwth-aachen.de>
2019-02-11 15:44:32 +0100

    Laten space visualization via t-SNE.

--- python/gps/visualization/latent_space.py
+++ python/gps/visualization/latent_space.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
+from sklearn.manifold import TSNE
 
 
 def visualize_latent_space(
@@ -24,20 +25,51 @@ def visualize_latent_space(
 
     N, dZ = z_mean.shape
 
-    x = np.linspace(-3, 3, 1000).reshape(1000, 1)
-    plt.plot(x, sp.stats.norm.pdf(x), color="black", linestyle=":", label='$\\mathcal{N}(0,1)$')
+    xs = np.linspace(-3, 3, 1000).reshape(1000, 1)
+    plt.plot(xs, sp.stats.norm.pdf(xs), color="black", linestyle=":", label='$\\mathcal{N}(0,1)$')
     for dim in range(dZ):
         # Fit GMM by hand
         gmm = GaussianMixture(N)
         gmm.means_ = z_mean[:, dim].reshape(N, 1)
         gmm.precisions_cholesky_ = (1 / z_std[:, dim]).reshape(N, 1, 1)
         gmm.weights_ = np.ones(N) / N
-        ax1.plot(x, np.exp(gmm.score_samples(x)), linewidth=1, label='$\\mathbf{z}[%d]$' % dim)
+        ax1.plot(xs, np.exp(gmm.score_samples(xs)), linewidth=1, label='$\\mathbf{z}[%d]$' % dim)
 
     ax1.legend()
-    fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
+
+    if file_name is not None:
+        fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
+        if export_data:
+            np.savez_compressed(file_name, z_mean=z_mean, z_std=z_std)
     if show:
         plt.show()
     plt.close(fig)
-    if export_data:
-        np.savez_compressed(file_name, z_mean=z_mean, z_std=z_std)
+
+def visualize_latent_space_tsne(file_name, z, color_map='gist_rainbow', show=False, export_data=True):
+    """
+    Visualizes latent space via tsne.
+    Args:
+        file_name: File name without extension.
+        Z: ndarray (N, S, dZ) with latent states were N is the number of input states and S the number of laten space sample of each state.
+        show: Display generated plot. This is a blocking operation.
+        export_data: Writes a npz file containing the plotted data points.
+                     This is useful for later recreation of the plot.
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.grid(linestyle=':')
+
+    N, S, dZ = z.shape
+    z_embedded = TSNE(n_components=2, perplexity=S).fit_transform(z.reshape(-1, dZ))
+
+    plt.scatter(z_embedded[:, 0], z_embedded[:, 1], c=np.repeat(np.arange(N), S), cmap=plt.cm.get_cmap(color_map, N))
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+
+    if file_name is not None:
+        fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
+        if export_data:
+            np.savez_compressed(file_name, z=z)
+    if show:
+        plt.show()
+    plt.close(fig)
