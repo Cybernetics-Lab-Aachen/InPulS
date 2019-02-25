1b702e8f64ce6d4747d1deb2cee3bc834751084c
Robin Kupper <robin.kupper@rwth-aachen.de>
2019-02-25 14:41:34 +0100

    Visualize sampled trajectories.

--- python/gps/visualization/__init__.py
+++ python/gps/visualization/__init__.py
@@ -1,4 +1,5 @@
-from gps.visualization.linear_model import visualize_linear_model
-from gps.visualization.loss import visualize_loss
 from gps.visualization.approximation import visualize_approximation
 from gps.visualization.latent_space import visualize_latent_space
+from gps.visualization.linear_model import visualize_linear_model
+from gps.visualization.loss import visualize_loss
+from gps.visualization.trajectory import visualize_trajectories
